c4014959ea16d926bd59c85dcf5c1e595e59893a
Nick Kohrn <bnkohrn@icloud.com>
2023-05-11 11:36:27 -0400

    Adds check for alternate icons support

--- Sources/AppIconsKit/AppIconsClient+DependencyKey.swift
+++ Sources/AppIconsKit/AppIconsClient+DependencyKey.swift
@@ -8,16 +8,16 @@ extension AppIconsClient: DependencyKey {
             UIApplication.shared.alternateIconName
         },
         selectIcon: { icon in
-            guard
-                UIApplication.shared.supportsAlternateIcons,
-                UIApplication.shared.alternateIconName != icon.name
-            else { return }
+            guard UIApplication.shared.alternateIconName != icon.name else { return }
             do {
                 try await UIApplication.shared.setAlternateIconName(icon.name)
             } catch {
                 // TODO: Log error
                 print("Updating icon to \(String(describing: icon.name)) failed: \(error.localizedDescription)")
             }
+        },
+        supportsAlternateIcons: {
+            UIApplication.shared.supportsAlternateIcons
         }
     )
 }
--- Sources/AppIconsKit/AppIconsClient.swift
+++ Sources/AppIconsKit/AppIconsClient.swift
@@ -3,4 +3,5 @@ import Foundation
 public struct AppIconsClient {
     public var alternateIconName: @MainActor @Sendable () async -> String?
     public var selectIcon: @MainActor @Sendable (AppIcon) async -> Void
+    public var supportsAlternateIcons: @MainActor @Sendable () -> Bool
 }
